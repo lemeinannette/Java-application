34ae0be306d1f1c672d1a22432881ebc563c77b1
Lemein <lemeinannette@gmail.com>
2023-10-16 11:38:22 +0300

    first commit

--- index.js
+++ index.js
@@ -1,59 +1,34 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+// Function to fetch and populate the coffee menu from JSON
+function fetchAndPopulateMenu() {
+    fetch('http://localhost:5000/menu') // Fetch the JSON file
+        .then((response) => response.json()) // Parse the JSON data
+        .then((data) => {
+            const menuList = document.getElementById('menu');
 
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
+            data.forEach((coffee) => {
+                const menuItem = document.createElement('li');
+                menuItem.innerHTML = `
+                    <span class="coffee-name">${coffee.name}</span> - ${coffee.description}
+                    <img src="${coffee.image}" alt="${coffee.name}" class="coffee-image">
+                    <button class="buy-button" onclick="buyCoffee('${coffee.name}')">Buy</button>
+                `;
 
-        if (targetElement) {
-            window.scrollTo({
-                behavior: 'smooth',
-                top: targetElement.offsetTop - 50, // Adjust scrolling position
+                menuList.appendChild(menuItem);
             });
-        }
-    });
+        })
+        .catch((error) => {
+            console.error('Error fetching coffee menu data:', error);
+        });
+}
+
+// JavaScript code for handling coffee purchases
+function buyCoffee(coffeeName) {
+    alert(`You have purchased a ${coffeeName}! Enjoy your coffee.`);
+    // You can add more functionality here, such as processing orders.
+}
+
+// Populate the coffee menu when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+    fetchAndPopulateMenu();
 });
 
-// Highlight the active section in the navigation
-window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    const sections = document.querySelectorAll('section');
-
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - 50; // Adjust top position
-        const sectionBottom = sectionTop + section.clientHeight;
-
-        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-            const sectionId = section.getAttribute('id');
-            const navLinks = document.querySelectorAll('nav a');
-            
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                if (link.getAttribute('href').substring(1) === sectionId) {
-                    link.classList.add('active');
-                }
-            });
-        }
-    });
-});
-
-// Display a message on clicking the menu items
-document.querySelectorAll('#menu li').forEach(menuItem => {
-    menuItem.addEventListener('click', function() {
-        const itemName = this.textContent;
-        alert(`You selected: ${itemName}`);
-    });
-});
-
-// Validate phone number input
-const phoneInput = document.querySelector('input[type="tel"]');
-phoneInput.addEventListener('input', function() {
-    const value = phoneInput.value;
-    const valid = /^\d{10}$/.test(value); // Valid phone number has 10 digits
-    if (!valid) {
-        phoneInput.setCustomValidity('Please enter a valid 10-digit phone number');
-    } else {
-        phoneInput.setCustomValidity('');
-    }
-});
